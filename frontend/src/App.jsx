b06d9cea3d630d89bc66b6d5d1a8537a2a352503
Calculuxx <aitboumaima@gmail.com>
2023-04-29 21:31:46 +0100

    did a simple front-end for the login and sign up page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "@/components/login";
 function App() {
   return <div className="app">
-    App
+     <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              isAuth ? (
+                <Navigate to="/chat" />
+              ) : (
+                <Login setUser={setUser} setSecret={setSecret} />
+              )
+            }
+          />
+          </Routes>
+     </BrowserRouter>
   </div>
 }
 
