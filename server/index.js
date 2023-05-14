cb5f52f312b9c6c48cb493966516724236245dbe
ilham Ab <ilhamab@MacBook-Air.local>
2023-05-14 18:20:27 +0100

    AI bot added

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import { Configuration, OpenAIApi } from "openai";
+import openAiRoutes from "./routes/openai.js";
 
 
 /* CONFIGURATIONS */
@@ -16,6 +18,17 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+
+/* OPEN AI CONFIGURATION */
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_API_KEY,
+});
+export const openai = new OpenAIApi(configuration);
+
+
+/* ROUTES */
+app.use("/openai", openAiRoutes);
 
 /* SERVER SETUP */
 const PORT = process.env.PORT || 9000;
